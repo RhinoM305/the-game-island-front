b95f41d775ab6afe9a138f55a5949c0c76f517d8
Ryan <morenoryan33@gmail.com>
2023-04-15 14:46:11 -0400

    added navbar container and button functions for navbar

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,39 +1,83 @@
 import thegameisland from "../content/images/thegameisland.png";
+import "../index.css";
+import React from "react";
+import $ from "jquery";
+import NavBarMenuDisplay from "./NavBarMenuDisplay.js";
 
 function NavBar() {
+  let menuWidth;
   return (
-    <div className="z-50 h-[155px] w-full backdrop-filter bg-opacity-40 backdrop-blur-lg bg-black fixed top-0 left-0">
-      <div className="flex items-center justify-between w-full h-full">
-        <img src={thegameisland} className="h-full mt-2 ml-4" />
-        <div className="flex mr-4 text-4xl text-white font-Titan-One">
-          <div className="flex">
-            <input
-              className="mx-2 w-[550px] border-2 rounded-lg border-white bg-transparent font-sans pl-1"
-              placeholder="Search our inventory!!"
-            />
-            <button
-              className="w-[50px] border-2 rounded-lg border-white text-white text-md bg-transparent p-2 mr-4
+    <>
+      <div className="z-[48] h-[155px] w-full backdrop-filter bg-opacity-40 backdrop-blur-lg bg-black fixed top-0 left-0">
+        <div className="flex items-center justify-between w-full h-full">
+          <img src={thegameisland} className="h-full w-[20%] mt-2 ml-4" />
+          <div className="flex w-[35%] mr-4 text-4xl text-white font-Titan-One">
+            <div className="flex w-full">
+              <input
+                className="mx-2 w-full border-2 rounded-lg border-white bg-transparent font-sans pl-1"
+                placeholder="Search our inventory!!"
+              />
+              <button
+                className="w-[50px] border-2 rounded-lg border-white text-white text-md bg-transparent p-2 mr-4
           "
-            >
-              <i className="text-white fa fa-search"></i>
-            </button>
+              >
+                <i className="text-white fa fa-search"></i>
+              </button>
+            </div>
           </div>
-          <button className="mx-2">
-            <i className="mr-1 fa fa-sitemap"></i>Collection
-          </button>
-
-          <button className="mx-2">
-            <i className="mr-1 fa fa-user"></i>Account
-          </button>
-          <button className="mx-2">
-            <i className="mr-1 text-white fa fa-rotate-left"></i>Recent
-          </button>
-          <button className="mx-2">
-            <i className="mr-1 fa fa-shopping-cart"></i>Cart
-          </button>
+          <div
+            id="mainNavMenu"
+            className="w-[50%] h-full text-white font-Titan-One flex text-5xl items-center"
+          >
+            <div
+              className="w-full text-center mx-2"
+              onClick={() => {
+                if ($("#menu-container").is(":visible")) {
+                  $("#menu-container").slideUp(250);
+                } else {
+                  $("#menu-container").slideDown(250);
+                }
+              }}
+            >
+              Collection
+            </div>
+            <div
+              className="w-full text-center mx-2"
+              onClick={() => {
+                if ($("#menu-container").is(":visible")) {
+                  $("#menu-container").slideUp(250);
+                } else {
+                  $("#menu-container").slideDown(250);
+                }
+              }}
+            >
+              Account
+            </div>
+            <div className="w-full text-center mx-2" onClick={() => {}}>
+              Recent
+            </div>
+            <div
+              className="w-full text-center mx-2"
+              onClick={() => {
+                if ($("#menu-container").is(":visible")) {
+                  $("#menu-container").slideUp(250);
+                } else {
+                  $("#menu-container").slideDown(250);
+                }
+              }}
+            >
+              Cart
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        id="menu-container"
+        className="fixed top-[154px] w-full h-1/2 z-[49] bg-white"
+      >
+        <NavBarMenuDisplay />
+      </div>
+    </>
   );
 }
 
---
+++ src/nav/NavBarMenuDisplay.js
@@ -0,0 +1,5 @@
+function NavBarMenuDisplay() {
+  <div className="w-full bg-purple-500"></div>;
+}
+
+export default NavBarMenuDisplay;
